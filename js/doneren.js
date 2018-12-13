fc9877309529c37bf1eeed567d723cdde6e36192
Sander Star <sanderstar@creetion.com>
2018-12-13 17:09:33 +0100

    test

--- js/doneren.js
+++ js/doneren.js
@@ -1,10 +1,17 @@
 $(function () {
+    // Keuze voor datum
+    $("#datumpicker").change(function (event) {
+        var selText = $(this).val();
+        $("#datum").val(selText);
+    });
+
     // Keuze voor doel
     $(".doel a").click(function (event) {
         var selText = $(this).text();
         $("#doel").val(selText);
     });
 
+    // Keuze voor bedrag
     $(".bedrag a").click(function (event) {
         var selText = $(this).find("img").prop("alt")
         $("#bedrag").val(selText);

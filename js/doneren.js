5d724e97e7c66d43e70a664d965dd82bb6c8eef0
Sander Star <sanderstar@creetion.com>
2018-12-13 10:51:12 +0100

    Dropdown voor bedrag keuze

--- js/doneren.js
+++ js/doneren.js
@@ -1,19 +1,8 @@
-console.log("nu");
-
-$(function() {
-    console.log("ready");
-
-    // Keuze in dropdown lijst gemaakt
-    $('#doeldropdown').on("hide.bs.dropdown", function (e) {
-        console.log("keuze " + e);
+$(function () {
+    // Keuze voor doel
+    $(".dropdown-menu a").click(function (event) {
+        console.log($(this).parent);
+        var selText = $(this).text();
+        $("#doel").val(selText);
     });
-
-    $(".dropdown-menu a").click(function() {
-        // TODO bezig
-        var keuze = $(this).parents(".btn-group").find('.selection').text($(this).text());
-        var waarde = $(this).parents(".btn-group").find('.selection').val($(this).text());
-        console.log(keuze + " " + waarde);
-     });
-  
-  
 });        
